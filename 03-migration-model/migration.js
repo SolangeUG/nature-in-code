97b01a439dc1212db6f8f7ccaaa274a51a870b1d
Solange U. Gasengayire <sgasengayire@gmail.com>
2019-04-01 00:15:01 +0100

    Refactor and cleanup migration model page and script

--- 03-migration-model/migration.js
+++ 03-migration-model/migration.js
@@ -1,62 +1,55 @@
-import { round } from 'helper';
-
-/********************************************************************************
- * 							SPATIAL MODEL GRID GENERATION						*
- *******************************************************************************/
-// Much needed global variables
-var p 					= 0.5;
-var a1a1				= 0;
-var a1a2				= 0;
-var a2a2				= 0;
-var grid 				= [];
-var grid_length 		= 100;
-var generation_counter	= 0;
-var max_mating_distance = 1;
+let a1a1 = 0;
+let a1a2 = 0;
+let a2a2 = 0;
+const p = 0.5;
+const grid = [];
+const grid_length = 100;
+let generation_counter = 0;
+const max_mating_distance = 1;
 
 /**
  * Initialize the spatial model grid
  */
 function init_grid() {
-	for (var i = 0; i < grid_length; i++) {
-		grid[i] = [];
-		for (var j = 0; j < grid_length; j++) {
-			var random_num = Math.random();
-			if (random_num < p * p) {
-				grid[i][j] = "A1A1";
-				a1a1++;
-			} else if (random_num > 1 - ((1 - p) * (1 - p))) {
-				grid[i][j] = "A2A2";
-				a2a2++;
-			} else {
-				grid[i][j] = "A1A2";
-				a1a2++;
-			}
-
-		}
-	}
-	//console.log("A1A1:", a1a1, "|", "A1A2:", a1a2, "|", "A2A2:", a2a2);
-	//console.log("generation", generation_counter, ":", a1a1, a1a2, a2a2);
+  for (let i = 0; i < grid_length; i++) {
+    grid[i] = [];
+    for (let j = 0; j < grid_length; j++) {
+      const probability = Math.random();
+      if (probability < p * p) {
+        grid[i][j] = "A1A1";
+        a1a1++;
+      } else if (probability > 1 - (1 - p) * (1 - p)) {
+        grid[i][j] = "A2A2";
+        a2a2++;
+      } else {
+        grid[i][j] = "A1A2";
+        a1a2++;
+      }
+    }
+  }
+  console.log("A1A1:", a1a1, "|", "A1A2:", a1a2, "|", "A2A2:", a2a2);
+  console.log("generation", generation_counter, ":", a1a1, a1a2, a2a2);
 }
 
 /**
  * Run the simulation for one generation
  */
 function run_generation() {
-	var temp_grid = [];
-	for (var i = 0; i < grid_length; i++) {
-		temp_grid[i] = [];
-		for (var j = 0; j < grid_length; j++) {
-			mating_partner = pick_mating_partner(i, j);
-			temp_grid[i][j] = get_offspring(grid[i][j], mating_partner);
-		}
-	}
-	for (var i = 0; i < grid_length; i++) {
-		for (var j = 0; j < grid_length; j++) {
-			grid[i][j] = temp_grid[i][j];
-		}
-	}
-	print_data();
-	generation_counter++;
+  let temp_grid = [];
+  for (let i = 0; i < grid_length; i++) {
+    temp_grid[i] = [];
+    for (let j = 0; j < grid_length; j++) {
+      let mating_partner = pick_mating_partner(i, j);
+      temp_grid[i][j] = get_offspring(grid[i][j], mating_partner);
+    }
+  }
+  for (let i = 0; i < grid_length; i++) {
+    for (let j = 0; j < grid_length; j++) {
+      grid[i][j] = temp_grid[i][j];
+    }
+  }
+  print_data();
+  generation_counter++;
 }
 
 /**
@@ -66,185 +59,113 @@ function run_generation() {
  * @return a valid position in the grid
  */
 function pick_mating_partner(position_i, position_j) {
-	var x = get_random_int(position_i - max_mating_distance, position_i + max_mating_distance);
-	var y = get_random_int(position_j - max_mating_distance, position_j + max_mating_distance);
-	x = get_bounded_index(x);
-	y = get_bounded_index(y);
-	return grid[x][y];
-}
-
-/**
- * Return a random integer betwen two values
- * @param min_value is the lower-bound value
- * @param max_value is the higher-bound value
- * @return a random value between min_value and max_value
- */
-function get_random_int(min_value, max_value) {
-	var offset = min_value;
-	var range = max_value - min_value + 1;
-	var result = offset + Math.floor(Math.random() * range);
-	return result;
-}
-
-/**
- * Return the correct bounded index from the current position
- * @param index is the current position
- * @return the bounded index which is assured to be on the grid
- */
-function get_bounded_index(index) {
-	var bounded_index = index;
-	if (index < 0) {
-		bounded_index  = index + grid_length;
-	}
-	if (index >= grid_length) {
-		bounded_index = index - grid_length;
-	}
-	return bounded_index;
+  let x = random_value_between(position_i - max_mating_distance, position_i + max_mating_distance);
+  let y = random_value_between(position_j - max_mating_distance, position_j + max_mating_distance);
+  x = bounded_value(x);
+  y = bounded_value(y);
+  return grid[x][y];
 }
 
 /**
  * Return an offspring from two given individuals
  * @param parent1 is the first individual
  * @param parent2 is the mating partner
- * @return the offspring via the Mendelian Inheritance
+ * @return string : the offspring via the Mendelian Inheritance
  */
- function get_offspring(parent1, parent2) {
- 	let probability = 0;
+function get_offspring(parent1, parent2) {
+  // both parents are homozygous, identical and of A1A1 genotype
+  if (parent1 === "A1A1" && parent2 === "A1A1") {
+    return "A1A1";
+  }
 
- 	if (homozygous_identical(parent1, parent2)) {
- 		return parent1;
- 	}
+  // one parent is heterozygous, and the other is homozygous of genotype A1A1
+  if ((parent1 === "A1A1" && parent2 === "A1A2")
+      || (parent1 === "A1A2" && parent2 === "A1A1")) {
+    if (Math.random() < 0.5) {
+      return "A1A1";
+    }
+    else {
+      return "A1A2";
+    }
+  }
 
- 	if ((homozygous(parent1) && heterozygous(parent2))
- 			|| (heterozygous(parent1) && homozygous(parent2)) {
- 		probability = Math.random();
- 		if (probability < 0.5) {
- 			return homozygous_genotype(parent1, parent2);
- 		} else {
- 			return "A1A2";
- 		}
- 	}
+  // both parents are homozygous but not identical
+  if ((parent1 === "A1A1" && parent2 === "A2A2") || (parent1 === "A2A2" && parent2 === "A1A1")) {
+    return "A1A2";
+  }
 
- 	if (homozygous_non_identical(parent1, parent2)) {
- 		return "A1A2";
- 	}
+  // both parents are heterozygous
+  if (parent1 === "A1A2" && parent2 === "A1A2") {
+    const probability = Math.random();
+    if (probability < 0.25) {
+      return "A1A1";
+    }
+    else if (probability > 0.75){
+      return "A2A2";
+    }
+    else {
+      return "A1A2";
+    }
+  }
 
- 	if (heterozygous_identical(parent1, parent2)) {
- 		probability = Math.random();
- 		if (random_num < 0.25) {
- 			return "A1A1";
- 		} else if (random_num > 0.75) {
- 			return "A2A2";
- 		} else {
- 			return "A1A2";
- 		}
- 	}
- }
+  // one parent is heterozygous, and the other is homozygous of genotype A2A2
+  if ((parent1 === "A1A2" && parent2 === "A2A2") || (parent1 === "A2A2" && parent2 === "A1A2")) {
+    if (Math.random() < 0.5) {
+      return "A1A2";
+    }
+    else {
+      return "A2A2";
+    }
+  }
 
- // are parents homozygous and identical
- function homozygous_identical(parent1, parent2) {
- 	return (identical(parent1, parent2) && parent1 === "A1A1")
-           || (identical(parent1, parent2) && parent1 === "A2A2");
- }
-
- // are parents homozygous but not identical
- function homozygous_non_identical(parent1, parent2) {
-   const non_identical = ! identical(parent1, parent2);
- 	return (non_identical && is_heterozygous(parent1))
-           || (non_identical && is_heterozygous(parent2));
- }
-
- // are both parents are heterozygous
- function heterozygous_identical(parent1, parent2) {
- 	return identical(parent1, parent2) && parent1 === "A1A2";
- }
-
- function homozygous(parent) {
- 	return parent === "A1A1" || parent === "A2A2";
- }
-
- function heterozygous(parent) {
- 	return parent === "A1A2";
- }
-
- // are parents of the same gynotype
- function identical(parent1, parent2) {
-   return parent1 === parent2;
- }
-
- // return a homozygous genotype from a parent
- function homozygous_genotype(parent1, parent2) {
- 	if is_homozygous(parent1) {
- 		return parent1;
- 	}
- 	return parent2;
- }
+  // both parents are homozygous, identical and of A2A2 genotype
+  if (parent1 === "A2A2" && parent2 === "A2A2") {
+    return "A2A2";
+  }
+}
 
 /**
- * Visualize the data computed so far
+ * Print the computed data to the console
  */
 function print_data() {
-	a1a1 = 0;
-	a1a2 = 0;
-	a2a2 = 0;
-	for (var i = 0; i < grid_length; i++) {
-		for (var j = 0; j < grid_length; j++) {
-			if (grid[i][j] == "A1A1") {
-				a1a1++;
-			} else if (grid[i][j] == "A1A2") {
-				a1a2++;
-			} else {
-				a2a2++;
-			}
-		}
-	}
-	console.log("generation", generation_counter, ":", a1a1, a1a2, a2a2);
+  a1a1 = 0;
+  a1a2 = 0;
+  a2a2 = 0;
+  for (let i = 0; i < grid_length; i++) {
+    for (let j = 0; j < grid_length; j++) {
+      if (grid[i][j] === "A1A1") {
+        a1a1++;
+      } else if (grid[i][j] === "A1A2") {
+        a1a2++;
+      } else {
+        a2a2++;
+      }
+    }
+  }
+  console.log("generation", generation_counter, ":", a1a1, a1a2, a2a2);
 
-	var N = a1a1 + a1a2 + a2a2;
-	var observed_heterozygocity = a1a2 / N;
-	var p = ((2 * a1a1) + a1a2) / (2 * N);
-	var expected_heterozygocity = 2 * p * (1 - p);
-	var F = (expected_heterozygocity - observed_heterozygocity) / expected_heterozygocity;
-	console.log("F =", F);
+  const N = a1a1 + a1a2 + a2a2;
+  const observed_heterozygocity = a1a2 / N;
+  const p = ((2 * a1a1) + a1a2) / (2 * N);
+  const expected_heterozygocity = 2 * p * (1 - p);
+  const F = (expected_heterozygocity - observed_heterozygocity) / expected_heterozygocity;
+  console.log("F =", F);
 }
 
-/**
- * Display the grid
- */
-function display_grid() {
-	console.log(" ");
-	var line = "";
-	for (var i = 0; i < grid_length; i++) {
-		for (var j = 0; j < grid_length; j++) {
-			line += grid[i][j] + " ";
-		}
-		console.log(line);
-		line = "";
-	}
-}
-
-/**
- * Run the simulation for a hundred generations
- */
-function run_simulation() {
-	for (var i = 0; i < 100; i++) {
-		run_generation();
-	}
-}
 
 // State of the grid BEFORE running the simulation
+const colors = ["A1A1", "#F4F6F6", "A1A2", "#FFCC33", "A2A2", "#DD4D00"];
 init_grid();
-display_grid();
-run_simulation();
-draw_grid(grid);
+draw_grid(grid, colors);
+
 
 /**
  * Run the simulation for a hundred generations
- * And visualize the simulation
+ * and visualize the simulation
  */
 function simulate_and_visualize() {
-	run_generation();
-	update_grid(grid);
+  run_generation();
+  update_grid(grid, colors);
 }
 
 // State of the grid AFTER running the simulation.
