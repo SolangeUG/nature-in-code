8f4c694a925e038bd19e5f81306cb5b17450d3bb
Solange U. Gasengayire <sgasengayire@gmail.com>
2019-04-01 00:09:30 +0100

    Refactor and cleanup the Hardy-Weinberg model script

--- 01-hardy-weinberg-model/hardy-weinberg.js
+++ 01-hardy-weinberg-model/hardy-weinberg.js
@@ -1,32 +1,31 @@
-import { round } from 'helper';
+let a1a1 = 0.15;
+let a2a2 = 0.35;
+let a1a2 = 1 - (a1a1 + a2a2);
 
-/*******************************************************************************
- * 									HARDY-WEINBERG MODEL						*
- ******************************************************************************/
-
-// generation 0 genotype frequencies
-const a1a1 = 0.15;
-const a2a2 = 0.35;
-const a1a2 = 1 - (a1a1 + a2a2);
-
-// allele frequencies
+// Allele frequencies
 const p = a1a1 + (a1a2 / 2);
 const q = 1 - p;
 
-// initial genotype frequencies
+// Initial genotype frequencies
 console.log("generation", 0, ":", a1a1, a2a2, a1a2);
 
-// calculating the Hardy-Weinberg model
-function hardy_weinberg_model() {
-	for (let i = 1; i <= 20; i++) {
-		genotype_new_generation();
-		console.log("generation", i, ":", a1a1, a2a2, a1a2);
-	}
+/**
+ * Calculate next generation data
+ */
+function next_generation() {
+  a1a1 = round_to_n_decimals(p * p, 2);
+  a2a2 = round_to_n_decimals(q * q, 2);
+  a1a2 = 2 * round_to_n_decimals(p * q, 2);
 }
 
-// calculating the next generation of genotype frequencies
-function genotype_new_generation() {
-	a1a1 = round(p * p, 2);
-	a2a2 = round(q * q, 2);
-	a1a2 = 2 * round(p * q, 2);
+/**
+ * Calculate the Hardy-Weinberg model
+ */
+function hardy_weinberg_model() {
+  for (let i = 1; i <= 20; i++) {
+    next_generation();
+    console.log("generation", i, ":", a1a1, a2a2, a1a2);
+  }
 }
+
+hardy_weinberg_model();
--- 01-hardy-weinberg-model/helper.js
+++
@@ -1,5 +0,0 @@
-// generic function to round numbers to n digits after the decimal point
-export function round(value, n) {
-	const shifter = Math.pow(10, n);
-	return Math.round(value * shifter) / shifter;
-}
